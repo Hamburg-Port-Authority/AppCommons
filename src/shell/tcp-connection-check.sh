7a0c8becae697695fe8afd8d60e03a4e1871f966
deB4SH <git@b4sh.de>
2023-08-31 10:46:09 +0200

    add: script seconds check

--- src/shell/tcp-connection-check.sh
+++ src/shell/tcp-connection-check.sh
@@ -2,20 +2,24 @@
 
 host_file=/tmp/tcp-connection-check/host_file.txt
 out_file=/tmp/tcp-results.txt
+REGEX="^(00|10|20|30|40|50)"
 
 while true; do
-    while read -r ip; do
-        #build date
-        fulld+=$(date +%d.%m.%y)
-        fulld+=" "
-        fulld+=$(date +%T)
-        #check connection
-        if timeout 5 bash -c "cat < /dev/null >/dev/tcp/${ip}/22"; then
-            echo -e "Success - $fulld - $ip - $NODE_NAME"
-        else
-            echo -e "Failure - $fulld - $ip - $NODE_NAME" 
-        fi >> "$out_file"
-        unset fulld
-    done < "$host_file"
-    sleep 10
+    CURRENT=$(date +%S)
+    if [[ $CURRENT =~ $REGEX ]]; then
+        while read -r ip; do
+            #build date
+            fulld+=$(date +%d.%m.%y)
+            fulld+=" "
+            fulld+=$(date +%T)
+            #check connection
+            if timeout 5 bash -c "cat < /dev/null >/dev/tcp/${ip}/22"; then
+                echo -e "Success - $fulld - $ip - $NODE_NAME"
+            else
+                echo -e "Failure - $fulld - $ip - $NODE_NAME" 
+            fi >> "$out_file"
+            unset fulld
+        done < "$host_file"
+    fi
+    sleep 1
 done
