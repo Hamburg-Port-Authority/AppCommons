764a4f666acefb5f838d188e960e03940e552420
deB4SH <git@b4sh.de>
2023-08-31 08:15:28 +0200

    add: tcp connection check script

---
+++ src/shell/tcp-connection-check.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+host_file=/tmp/tcp-connection-check/host_file.txt
+out_file=/tmp/tcp-connection-check/results.txt
+
+while true; do
+    while read -r ip; do
+        date=$(date +%Y%m%d_%H%M%S)
+        if timeout 5 bash -c "cat < /dev/null >/dev/tcp/${ip}/22"; then
+            echo -e "$date - $ip \t Success"
+        else
+            echo -e "$date - $ip \t Failure" 
+        fi >> "$out_file"
+    done < "$host_file"
+    sleep 10
+done
