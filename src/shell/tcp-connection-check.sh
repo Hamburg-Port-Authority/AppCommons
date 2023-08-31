9bbbeeb99b8b33721ff1df8fb9ec8c72b37b940b
deB4SH <git@b4sh.de>
2023-08-31 08:18:29 +0200

    minor cleanups

--- src/shell/tcp-connection-check.sh
+++ src/shell/tcp-connection-check.sh
@@ -7,9 +7,9 @@ while true; do
     while read -r ip; do
         date=$(date +%Y%m%d_%H%M%S)
         if timeout 5 bash -c "cat < /dev/null >/dev/tcp/${ip}/22"; then
-            echo -e "$date - $ip \t Success"
+            echo -e "$date - $ip - Success"
         else
-            echo -e "$date - $ip \t Failure" 
+            echo -e "$date - $ip - Failure" 
         fi >> "$out_file"
     done < "$host_file"
     sleep 10
